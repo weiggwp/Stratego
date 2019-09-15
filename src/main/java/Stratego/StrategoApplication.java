014a56723a8d8a1669551bcae168d6bbe47d6f90
Ying Zhang <yingz1985@yahoo.com>
2019-09-15 02:07:54 -0400

    board and pieces done

---
+++ src/main/java/Stratego/StrategoApplication.java
@@ -0,0 +1,15 @@
+package Stratego;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class StrategoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrategoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/Stratego/controller/boardController.java
@@ -0,0 +1,28 @@
+package Stratego.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import Stratego.board.arrangement;
+
+
+@Controller
+public class boardController {
+
+    @GetMapping("/board")
+    public String greeting(Model model)
+    {
+        int count = 10;
+        int inner = 10;
+        boardController control = new boardController();
+        //render board.html
+        model.addAttribute("count", count);
+        model.addAttribute("inner",inner);
+
+        model.addAttribute("pos",new arrangement());
+
+        return "board";
+    }
+
+
+}
