3e1e3e3325ced27f833d922c5611a7a2a8859a71
weiggwp <wei.chen.8@stonybrook.edu>
2019-09-15 20:11:32 -0400

    register page loaded, trying to connect to db

--- src/main/java/Stratego/RegistrationController.java
+++ src/main/java/Stratego/RegistrationController.java
@@ -2,18 +2,18 @@ package Stratego;
 
 import javax.validation.Valid;
 
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.stereotype.Controller;
-        import org.springframework.ui.Model;
-        import org.springframework.validation.BindingResult;
-        import org.springframework.web.bind.annotation.GetMapping;
-        import org.springframework.web.bind.annotation.ModelAttribute;
-        import org.springframework.web.bind.annotation.PostMapping;
-        import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-        import Stratego.model.User;
-        import Stratego.service.UserService;
-        import Stratego.UserDto;
+import Stratego.model.User;
+import Stratego.service.UserService;
+import Stratego.UserDto;
 
 @Controller
 @RequestMapping("/register")
